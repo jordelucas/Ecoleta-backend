f6d7e2b63a615853f5b72ee6e1270f79824d0e7d
jordeva.silva <jordevalucas@gmail.com>
2020-08-01 12:49:33 -0300

    cria pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,50 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         };
     })
 
     return response.json(serializedItems)
+})
+
+routes.post('/points', async (request, response) => {
+    const { 
+        name,
+        email, 
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const insertedIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email, 
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: Number) => {
+        return {
+            item_id,
+            point_id: point_id
+        }
+    })
+
+    await knex('point_items').insert(pointItems)
+
+    return response.json({ success: true })
 })
 
 export default routes;
