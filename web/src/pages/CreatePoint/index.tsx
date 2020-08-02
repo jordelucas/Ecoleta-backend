b8adf334e08be33073cd903173e9bee99a0d4dec
Jordevá Lucas <jordevalucas@hotmail.com>
2020-08-02 17:37:38 -0300

    busca itens de coleta da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg'
 import './styles.css'
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([])
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    })
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -95,30 +110,12 @@ const CreatePoint: React.FC = () => {
           </legend>
 
           <ul className="items-grid">
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-              <span>Lâmpada</span>
-            </li>
+            {items.map(item => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title}/>
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
       
